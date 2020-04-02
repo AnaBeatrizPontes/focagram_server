5e9cd811b3fba015367dfb8517fa39d7c907e4f0
AnaPontes <anabeatrizsantospontes@gmail.com>
2020-04-01 22:58:54 -0300

    :lock: Implemta a autenticação de usuário
    Implementa a criação de usuário e a autenticação de login.
    
    referências: https://medium.com/binar-academy/rails-api-jwt-authentication-a04503ea3248
    	     https://diegod.netlify.com/

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount ActionCable.server => '/cable'
+
+  scope "/", defaults: { format: :json} do
+    resources :users, except: [:index, :destroy]
+    resources :post
+    resources :comment, only: [:create]
+
+    post '/auth/login', to: 'authentication#login'
+  end
 end
